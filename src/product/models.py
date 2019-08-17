55ce7c20881ec71b6f8fc638c6649f875499ae72
antonnifo <antonnifo@live.com>
2019-08-18 00:00:05 +0300

    add owner field

--- src/product/models.py
+++ src/product/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Products(models.Model):
 
@@ -12,6 +13,7 @@ class Products(models.Model):
     )
     
     name = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField(max_length=500)
     condition  = models.CharField(max_length=100, choices=CONDITION_TYPE)
     price = models.DecimalField(max_digits=5,decimal_places=2)
