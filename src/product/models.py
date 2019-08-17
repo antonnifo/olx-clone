3a8fee750508e3de9a8980cf664a55a057cd0422
antonnifo <antonnifo@live.com>
2019-08-17 23:37:28 +0300

    add choices for condition of product

--- src/product/models.py
+++ src/product/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
 class Products(models.Model):
+
+    CONDITION_TYPE = (
+        ("New","New"),
+        ("Used for less than a Month","Used for less than a Month"),
+        ("Used for less than  3 Months","Used for less than 3 Months"),
+        ("Used for less than  6 Months","Used for less than 6 Months"),
+        ("Used for more than  6 Months","Used for more than 6 Months")
+        
+    )
     
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
-    condition  = models.CharField(max_length=100)
+    condition  = models.CharField(max_length=100, choices=CONDITION_TYPE)
     price = models.DecimalField(max_digits=5,decimal_places=2)
     created = models.DateTimeField()
