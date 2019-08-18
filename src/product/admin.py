e327c20e6e221ef950ca646676879ae6a6a2c684
antonnifo <antonnifo@live.com>
2019-08-18 12:02:52 +0300

    regester brand n product_images models to admin site

--- src/product/admin.py
+++ src/product/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import Products,Category
+from .models import Products,Category,Brand,ProductImage
+
 admin.site.register(Products)
 admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(ProductImage)
