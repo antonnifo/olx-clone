a4154fd0221427d63216090fabfec99830714cdc
antonnifo <antonnifo@live.com>
2019-08-22 16:14:26 +0300

    pass product images

--- src/product/views.py
+++ src/product/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Products
+from .models import Products, ProductImage
 
 def product_list(request):
 
@@ -17,10 +17,13 @@ def product_detail(request,product_slug):
 
     product_detail = Products.objects.get(slug=product_slug)
 
+    Product_image  = ProductImage.objects.filter(product=product_detail)
+
     template       = 'Products/product_detail.html'
 
     context        = {
-                        'product_detail' : product_detail
+                        'product_detail' : product_detail,
+                        'product_image'  : Product_image
     }
 
     return render(request, template, context)
