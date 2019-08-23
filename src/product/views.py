3b26427f6bbc2f295f847064ad75e110e9bbef86
antonnifo <antonnifo@live.com>
2019-08-23 14:10:58 +0300

    search database using Q

--- src/product/views.py
+++ src/product/views.py
@@ -1,30 +1,40 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
-from django.db.models import Count
+from django.db.models import Count, Q
 
 from .models import ProductImage, Products, Category
 
 
 def product_list(request, category_slug=None):
 
-    category      = None 
-    product_list  = Products.objects.all()
-    category_list = Category.objects.annotate(total_products=Count('products'))
+    category         = None 
+    product_list     = Products.objects.all()
+    category_list    = Category.objects.annotate(total_products=Count('products'))
     
     if category_slug:
-        category  = Category.objects.get(slug=category_slug)
-        product_list =  product_list.filter(category=category)
+        category     = Category.objects.get(slug   =category_slug)
+        product_list = product_list.filter(category=category)
 
-    
+    search_query     = request.GET.get('q')
 
-    paginator     = Paginator(product_list, 1)
-    page          = request.GET.get('page')
-    product_list  = paginator.get_page(page) 
-    template      = 'Products/product_list.html'
-    context       = {
-                    'product_list'  : product_list,
-                    'category_list' : category_list,
-                    'category'      : category
+    if search_query:
+        product_list = product_list.filter(
+            Q(name__icontains                           = search_query) |
+            Q(condition__icontains                      = search_query) |
+            Q(description__icontains                    = search_query) |
+            Q(brand__brand_name__icontains              = search_query) |
+            Q(category__category_name__icontains        = search_query)
+        )
+
+
+    paginator        = Paginator(product_list, 6)
+    page             = request.GET.get('page')
+    product_list     = paginator.get_page(page) 
+    template         = 'Products/product_list.html'
+    context          = {
+                        'product_list'  : product_list,
+                        'category_list' : category_list,
+                        'category'      : category
         }
 
     return render(request ,template, context)
