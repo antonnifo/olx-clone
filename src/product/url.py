fa5441f7e01f7787671737e0f940d8992375053f
antonnifo <antonnifo@live.com>
2019-08-23 14:48:19 +0300

    fix conflicting urls

--- src/product/url.py
+++ src/product/url.py
@@ -5,5 +5,5 @@ app_name = 'product'
 urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('<str:category_slug>', views.product_list, name='product_list_category'),
-    path('<str:product_slug>', views.product_detail, name='product_detail')
+    path('detail/<str:product_slug>', views.product_detail, name='product_detail')
 ]
