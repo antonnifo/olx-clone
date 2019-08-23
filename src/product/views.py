2e745184cf98ff5452d21871f93a7901afcd7d40
antonnifo <antonnifo@live.com>
2019-08-23 09:10:45 +0300

    add category slug in productlist view

--- src/product/views.py
+++ src/product/views.py
@@ -5,23 +5,26 @@ from django.db.models import Count
 from .models import ProductImage, Products, Category
 
 
-def product_list(request):
+def product_list(request, category_slug=None):
 
+    category      = None 
     product_list  = Products.objects.all()
-
     category_list = Category.objects.annotate(total_products=Count('products'))
+    
+    if category_slug:
+        category  = Category.objects.get(slug=category_slug)
+        product_list =  product_list.filter(category=category)
 
-    template      = 'Products/product_list.html'
+    
 
     paginator     = Paginator(product_list, 1)
-
     page          = request.GET.get('page')
-
     product_list  = paginator.get_page(page) 
-
+    template      = 'Products/product_list.html'
     context       = {
                     'product_list'  : product_list,
-                    'category_list' : category_list
+                    'category_list' : category_list,
+                    'category'      : category
         }
 
     return render(request ,template, context)
