0191435b9955065c820a240fafcff3bb25cc8814
antonnifo <antonnifo@live.com>
2019-08-23 09:07:37 +0300

    add url for categories

--- src/product/url.py
+++ src/product/url.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'product'
 urlpatterns = [
     path('', views.product_list, name='product_list'),
+    path('<str:category_slug>', views.product_list, name='product_list_category'),
     path('<str:product_slug>', views.product_detail, name='product_detail')
 ]
