8950c57bbcd0bcb7df0147c0503dd5a39636dbb2
antonnifo <antonnifo@live.com>
2019-08-31 18:41:59 +0300

    fix import error for products class

--- src/home/views.py
+++ src/home/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from product.models import Product , Category
+from product.models import Products , Category
 # Create your views here.
 
 def home(request):
     
     all_category = Category.objects.all() 
-    products = Product.objects.all()
+    products = Products.objects.all()
 
     template = 'home.html'
     context = { 'all_category' : all_category , 'products' : products}
